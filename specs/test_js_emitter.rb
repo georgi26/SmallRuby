f323a21ba6cafc006418962e4133fe70d4d70383
Georgi Georgiev <georgi.g26@gmail.com>
2023-09-14 21:20:15 +0300

    change test values

--- specs/test_js_emitter.rb
+++ specs/test_js_emitter.rb
@@ -2,16 +2,16 @@ describe SR::JSEmitter do
   describe "When we give SR class with method" do
     before do
       @src = "
-            x = 1 + 1
+            x = 879 + 67
         "
     end
     it "Must be translated to correct JS" do
       result = SR.transpileToJS(@src)
-      assert_equal "let x = new SRIntegerInstance(1).send(\"+\",[new SRIntegerInstance(1)])", result
+      assert_equal "let x = new SRIntegerInstance(879).send(\"+\",[new SRIntegerInstance(67)])", result
       baseJS = SR::JSEmitter.baseJS
       baseJS << "\n" << result << "\n console.log(x.variables[\"value\"])"
       output = `echo '#{baseJS}' | node `
-      assert_equal "2\n", output
+      assert_equal "946\n", output
     end
   end
 end
